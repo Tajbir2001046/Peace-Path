2e2ef3ddd47a8668393fc9e6daa19cffc7f88116
Tajbir2001046 <2001046@iot.bdu.ac.bd>
2024-11-18 08:50:53 +0600

    This is our project frontend part,Though it is not ready yet,We are working on it

---
+++ python/multipilacation.py
@@ -0,0 +1,9 @@
+# Function to multiply two numbers
+def multiply_numbers(a, b):
+    return a * b
+
+# Example usage
+num1 = 5
+num2 = 3
+result = multiply_numbers(num1, num2)
+print(f"The multiplication of {num1} and {num2} is: {result}")
